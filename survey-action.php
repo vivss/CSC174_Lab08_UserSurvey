6899c40b0c2c31491350a2b7ab5a6e0a6c758607
Ben Chomsang <schomsan@u.rochester.edu>
2017-11-13 17:42:42 -0500

    edited the radio button and checkbox attributes so they work with the php

--- survey-action.php
+++ survey-action.php
@@ -1,4 +1,23 @@
 <?php
 include 'connection.php';
 
+$robot_name = Trim(stripslashes($_POST["fname"]));
+$robot_name = Trim(stripslashes($_POST["lname"]));
+$email = Trim(stripslashes($_POST["email"]));
+
+
+
+$robot_name = mysqli_real_escape_string($connection, $robot_name);
+$email = mysqli_real_escape_string($connection, $email);
+
+
+$query  = "INSERT INTO robots";
+$query .= " ( Name, Email";
+$query .= ") VALUES (";
+$query .= "  '{$robot_name}', '{$email}'";
+$query .= ")";
+
+$result = mysqli_query($connection, $query);
+
+
 ?>
