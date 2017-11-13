43e200ffc546f0448af14880330ef35a573d84bd
Ben Chomsang <schomsan@u.rochester.edu>
2017-11-13 13:59:14 -0500

    added db credentials

--- admin.php
+++ admin.php
@@ -1,9 +1,9 @@
 <?php
 	// 1. Create a database connection
-	$dbhost = "localhost";
-	$dbuser = "";
-	$dbpass = "";
-	$dbname = "";
+	$dbhost = "66.147.242.186";
+	$dbuser = "urcscon3_bangkok";
+	$dbpass = "coffee1N";
+	$dbname = "urcscon3_bangkok";
 
 	$connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
 
