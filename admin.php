fa3011fe1e670887f73b356e1e9fa6b0161fdcbb
Ben Chomsang <schomsan@u.rochester.edu>
2017-11-13 17:34:04 -0500

    added survey-action.php and connection.php

--- admin.php
+++ admin.php
@@ -1,11 +1,5 @@
 <?php
-	// 1. Create a database connection
-	$dbhost = "66.147.242.186";
-	$dbuser = "urcscon3_bangkok";
-	$dbpass = "coffee1N";
-	$dbname = "urcscon3_bangkok";
-
-	$connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+	include 'connection.php';
 
 
 	// 2. Perform database query
---
+++ connection.php
@@ -0,0 +1,9 @@
+<?php
+	$dbhost = "66.147.242.186";
+	$dbuser = "urcscon3_bangkok";
+	$dbpass = "coffee1N";
+	$dbname = "urcscon3_bangkok";
+
+    $connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+    
+    ?>
